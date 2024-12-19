b1a86c1b3602dd9ed65349ff12313e8fc36abe71
Chirana <engsw.thiago@gmail.com>
2024-12-19 01:53:55 -0300

    fix: adjust response

--- app/controllers/donates_controller.rb
+++ app/controllers/donates_controller.rb
@@ -5,22 +5,21 @@ class DonatesController < ApplicationController
     begin
       ActiveRecord::Base.transaction do
         @donate = Donate.create!(donate_params)
-
         donate_generated = EfipayService.gen_new_payment(@donate)
+        pix_copia_cola = donate_generated[:pix_copia_cola]
 
-        qrcode = RQRCode::QRCode.new(donate_generated[:pix_copia_cola])
+        qrcode = RQRCode::QRCode.new(pix_copia_cola)
         qr_svg = qrcode.as_svg(
           color: "000",                   # Cor do QR Code
           fill: "FFF",                    # Cor de fundo
           module_size: 8,                 # Tamanho de cada módulo
-          shape_rendering: "crispEdges",
           use_path: true,                 # Renderizar com <path> para SVG menor
           viewbox: true                   # Ativa o atributo viewBox para responsividade
-        )
+        ).to_s.gsub("<?xml version=\"1.0\" standalone=\"yes\"?>", "").html_safe
 
         render json: {
-          donate: donate_generated,
-          qr_code: qr_svg
+          pix_copia_cola:,
+          qr_code_svg: qr_svg
         }
       end
     rescue ActiveRecord::RecordInvalid => e
